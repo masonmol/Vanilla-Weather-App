2407cbb08ea0c6a05cd7a947480dbb8af4ee7939
masonmol <morganlmason96@gmail.com>
2021-09-12 10:52:52 -0400

    Updated time and data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,68 @@
+let dateElement = document.querySelector(".date");
+
+let currentDate = new Date();
+
+let day = currentDate.getDay();
+let month = currentDate.getMonth();
+let date = currentDate.getDate();
+let year = currentDate.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let formattedDate = `${days[day]}, ${months[month]} ${date}, ${year}`;
+
+dateElement.innerHTML = formattedDate;
+
+function convertToStandardTime(hour, minute) {
+  let meridiem;
+
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+
+  if (hour >= 12) {
+    meridiem = "pm";
+  } else {
+    meridiem = "am";
+  }
+
+  if (hour > 12) {
+    return `${hour - 12}:${minute} ${meridiem}`;
+  } else {
+    return `${hour}:${minute} ${meridiem}`;
+  }
+}
+
+let timeElement = document.querySelector(".time");
+
+let hour = currentDate.getHours();
+let minute = currentDate.getMinutes();
+
+let formattedTime = convertToStandardTime(hour, minute);
+timeElement.innerHTML = formattedTime;
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector(".temperature");
@@ -6,6 +71,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector(".humidity");
   let windElement = document.querySelector(".wind");
   let realTempElement = document.querySelector(".feels-like-temp");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionsElement.innerHTML = response.data.weather[0].description;
@@ -15,6 +81,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "eda5f4c1faef5ba99e914999cfcb1292";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Napa";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
