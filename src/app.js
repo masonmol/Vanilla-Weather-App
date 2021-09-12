05b6400381ed073e70ad1b6a964d7e8e73b79bdc
masonmol <morganlmason96@gmail.com>
2021-09-12 10:16:54 -0400

    Updated to current temperature data

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector(".temperature");
+  let cityElement = document.querySelector(".city");
+  let conditionsElement = document.querySelector(".conditions");
+  let humidityElement = document.querySelector(".humidity");
+  let windElement = document.querySelector(".wind");
+  let realTempElement = document.querySelector(".feels-like-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  conditionsElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  realTempElement.innerHTML = Math.round(response.data.main.feels_like);
+}
+
 let apiKey = "eda5f4c1faef5ba99e914999cfcb1292";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
