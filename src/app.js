e99bba012d074b0209e6fd841b780df5dbfc4803
masonmol <morganlmason96@gmail.com>
2021-09-12 20:38:04 -0400

    Added high/low conversion

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-let dateElement = document.querySelector(".date");
-
 let currentDate = new Date();
+let dateElement = document.querySelector(".date");
 
 let day = currentDate.getDay();
 let month = currentDate.getMonth();
@@ -68,16 +67,22 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector(".temperature");
   let cityElement = document.querySelector(".city");
   let conditionsElement = document.querySelector(".conditions");
+  let highTempElement = document.querySelector(".temp-high");
+  let lowTempElement = document.querySelector(".temp-low");
   let humidityElement = document.querySelector(".humidity");
   let windElement = document.querySelector(".wind");
   let realTempElement = document.querySelector(".feels-like-temp");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
+  celsiusHigh = response.data.main.temp_max;
+  celsiusLow = response.data.main.temp_min;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   conditionsElement.innerHTML = response.data.weather[0].description;
+  highTempElement.innerHTML = Math.round(celsiusHigh);
+  lowTempElement.innerHTML = Math.round(celsiusLow);
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   realTempElement.innerHTML = Math.round(response.data.main.feels_like);
@@ -107,7 +112,13 @@ function displayFahrenheitTemperature(event) {
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let fahrenheitTemperatureHigh = (celsiusHigh * 9) / 5 + 32;
+  let fahrenheitTemperatureLow = (celsiusLow * 9) / 5 + 32;
+  let highTempElement = document.querySelector(".temp-high");
+  let lowTempElement = document.querySelector(".temp-low");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  highTempElement.innerHTML = Math.round(fahrenheitTemperatureHigh);
+  lowTempElement.innerHTML = Math.round(fahrenheitTemperatureLow);
 }
 
 function displayCelsiusTemperature(event) {
@@ -115,7 +126,11 @@ function displayCelsiusTemperature(event) {
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector(".temperature");
+  let highTempElement = document.querySelector(".temp-high");
+  let lowTempElement = document.querySelector(".temp-low");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  highTempElement.innerHTML = Math.round(celsiusHigh);
+  lowTempElement.innerHTML = Math.round(celsiusLow);
 }
 
 let celciusTemperature = null;
